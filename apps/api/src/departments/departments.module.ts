e29ea9487a2dacb7866332c4921f29a2952f0cf9
Tee-star-black <tntshabeleng23@gmail.com>
2026-08-17 22:41:31 +0200

    feat: register department hierarchy service

--- apps/api/src/departments/departments.module.ts
+++ apps/api/src/departments/departments.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
+import { DepartmentStructureService } from './department-structure.service';
 import { DepartmentsController } from './departments.controller';
 import { DepartmentsService } from './departments.service';
 
 @Module({
   imports: [AuthModule],
   controllers: [DepartmentsController],
-  providers: [DepartmentsService],
+  providers: [DepartmentsService, DepartmentStructureService],
 })
 export class DepartmentsModule {}
