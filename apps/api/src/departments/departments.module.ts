7273c9c4894c34ca9f5b3a94a7d51b26b3bc9bb1
Tee-star-black <tntshabeleng23@gmail.com>
2026-08-17 22:14:13 +0200

    feat: scope organisation hierarchy

--- apps/api/src/departments/departments.module.ts
+++ apps/api/src/departments/departments.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module';
 import { DepartmentsController } from './departments.controller';
 import { DepartmentsService } from './departments.service';
 
 @Module({
+  imports: [AuthModule],
   controllers: [DepartmentsController],
   providers: [DepartmentsService],
 })
