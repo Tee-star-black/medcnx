ef4c2a5b6410d4712b9214e7ebe1eea025bcdc81
Tee-star-black <tntshabeleng23@gmail.com>
2026-08-18 11:11:37 +0200

    feat: register position history service

--- apps/api/src/positions/positions.module.ts
+++ apps/api/src/positions/positions.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
+import { PositionHistoryService } from './position-history.service';
 import { PositionsController } from './positions.controller';
 import { PositionsService } from './positions.service';
 
 @Module({
   imports: [AuthModule],
   controllers: [PositionsController],
-  providers: [PositionsService],
+  providers: [PositionsService, PositionHistoryService],
   exports: [PositionsService],
 })
 export class PositionsModule {}
