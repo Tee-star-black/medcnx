baac1e79aa294708270de14f8ab3714db4d23f33
Tee-star-black <tntshabeleng23@gmail.com>
2026-08-18 19:07:23 +0200

    feat: register manager attendance service

--- apps/api/src/attendance/attendance.module.ts
+++ apps/api/src/attendance/attendance.module.ts
@@ -3,10 +3,11 @@ import { AuthModule } from '../auth/auth.module';
 import { EmployeeSelfServiceModule } from '../employee-self-service/employee-self-service.module';
 import { AttendanceController } from './attendance.controller';
 import { AttendanceService } from './attendance.service';
+import { ManagerAttendanceService } from './manager-attendance.service';
 
 @Module({
   imports: [AuthModule, EmployeeSelfServiceModule],
   controllers: [AttendanceController],
-  providers: [AttendanceService],
+  providers: [AttendanceService, ManagerAttendanceService],
 })
 export class AttendanceModule {}
