c01eede0e165e75e15c17a953b8ee1fdc66f7db0
Tee-star-black <tntshabeleng23@gmail.com>
2026-08-18 16:31:55 +0200

    feat: register employee manager service

--- apps/api/src/employees/employees.module.ts
+++ apps/api/src/employees/employees.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
 import { EmployeeSelfServiceModule } from '../employee-self-service/employee-self-service.module';
 import { EmployeeLifecycleService } from './employee-lifecycle.service';
+import { EmployeeManagerService } from './employee-manager.service';
 import { EmployeesController } from './employees.controller';
 import { EmployeesService } from './employees.service';
 
 @Module({
   imports: [AuthModule, EmployeeSelfServiceModule],
   controllers: [EmployeesController],
-  providers: [EmployeesService, EmployeeLifecycleService],
+  providers: [EmployeesService, EmployeeLifecycleService, EmployeeManagerService],
 })
 export class EmployeesModule {}
