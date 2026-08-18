698b0cd2727841dc8d99c7fdb2098bdd0c67faa0
Tee-star-black <tntshabeleng23@gmail.com>
2026-08-18 09:15:20 +0200

    feat: register recruitment hiring service

--- apps/api/src/recruitment/recruitment.module.ts
+++ apps/api/src/recruitment/recruitment.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { RecruitmentController } from './recruitment.controller';
+import { RecruitmentHiringService } from './recruitment-hiring.service';
 import { RecruitmentService } from './recruitment.service';
 
 @Module({
   controllers: [RecruitmentController],
-  providers: [RecruitmentService],
+  providers: [RecruitmentService, RecruitmentHiringService],
 })
 export class RecruitmentModule {}
