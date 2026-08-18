5970777577f817e13264da81524768f9d2819794
Tee-star-black <tntshabeleng23@gmail.com>
2026-08-18 19:16:19 +0200

    feat: register manager performance service

--- apps/api/src/performance/performance.module.ts
+++ apps/api/src/performance/performance.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from '../auth/auth.module';
+import { ManagerPerformanceService } from './manager-performance.service';
 import { PerformanceController } from './performance.controller';
 import { PerformanceService } from './performance.service';
 
 @Module({
   imports: [AuthModule],
   controllers: [PerformanceController],
-  providers: [PerformanceService],
+  providers: [PerformanceService, ManagerPerformanceService],
 })
 export class PerformanceModule {}
